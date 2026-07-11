78fbf6c3d400938130a4e22e5a18621b835578f3
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2026-07-11 17:02:48 -0300

    fix: ajustando erro de teste

--- ms-finance-api/src/test/java/dev/vieira/ms_finance_api/MsFinanceApiApplicationTests.java
+++ ms-finance-api/src/test/java/dev/vieira/ms_finance_api/MsFinanceApiApplicationTests.java
@@ -2,9 +2,20 @@ package dev.vieira.ms_finance_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class MsFinanceApiApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
 
 	@Test
 	void contextLoads() {
