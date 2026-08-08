c94111b38fc6fd3fcb25944c09233cdea0e5dc3c
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2026-08-08 12:26:57 -0300

    teste

--- ms-notification-api/src/test/java/dev/vieira/ms_notification_api/MsNotificationApiApplicationTests.java
+++ ms-notification-api/src/test/java/dev/vieira/ms_notification_api/MsNotificationApiApplicationTests.java
@@ -2,8 +2,10 @@ package dev.vieira.ms_notification_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class MsNotificationApiApplicationTests {
 
 	@Test
