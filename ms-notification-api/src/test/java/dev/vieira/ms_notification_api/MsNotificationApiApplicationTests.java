338b13997ac799878f0e7d54bf9cbb645258332c
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2026-08-27 19:08:14 -0300

    fix: ajustando config do projeto

--- ms-notification-api/src/test/java/dev/vieira/ms_notification_api/MsNotificationApiApplicationTests.java
+++ ms-notification-api/src/test/java/dev/vieira/ms_notification_api/MsNotificationApiApplicationTests.java
@@ -3,9 +3,13 @@ package dev.vieira.ms_notification_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("local")
+@TestPropertySource(properties = {
+		"telegram_token=fake-token",
+		"spring.rabbitmq.addresses=amqp://guest:guest@localhost:5672"
+})
 class MsNotificationApiApplicationTests {
 
 	@Test
