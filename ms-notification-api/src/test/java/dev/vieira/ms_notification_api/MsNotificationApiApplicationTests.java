93a76fea89cbcf07cea0497102e251cc6f252c83
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2026-08-27 19:14:41 -0300

    fix: ajustando config do projeto

--- ms-notification-api/src/test/java/dev/vieira/ms_notification_api/MsNotificationApiApplicationTests.java
+++ ms-notification-api/src/test/java/dev/vieira/ms_notification_api/MsNotificationApiApplicationTests.java
@@ -1,9 +1,11 @@
 package dev.vieira.ms_notification_api;
 
+import dev.vieira.ms_notification_api.service.MessageService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @TestPropertySource(properties = {
@@ -11,6 +13,9 @@ import org.springframework.test.context.TestPropertySource;
 		"spring.rabbitmq.addresses=amqp://guest:guest@localhost:5672"
 })
 class MsNotificationApiApplicationTests {
+
+	@MockitoBean
+	MessageService messageService;
 
 	@Test
 	void contextLoads() {
